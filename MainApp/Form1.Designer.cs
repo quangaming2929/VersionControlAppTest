c28ebc0aef3dcbbab6779dc71dbf24633eadbc03
quangaming2929 <quangaming2929@DESKTOP-AJC0EMN>
2019-01-09 18:53:23 +0700

    Create

--- MainApp/Form1.Designer.cs
+++ MainApp/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbVersion = new System.Windows.Forms.Label();
+            this.btnCheckForUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbVersion
+            // 
+            this.lbVersion.AutoSize = true;
+            this.lbVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVersion.Location = new System.Drawing.Point(338, 79);
+            this.lbVersion.Name = "lbVersion";
+            this.lbVersion.Size = new System.Drawing.Size(45, 29);
+            this.lbVersion.TabIndex = 0;
+            this.lbVersion.Text = "1.0";
+            // 
+            // btnCheckForUpdate
+            // 
+            this.btnCheckForUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckForUpdate.Location = new System.Drawing.Point(239, 187);
+            this.btnCheckForUpdate.Name = "btnCheckForUpdate";
+            this.btnCheckForUpdate.Size = new System.Drawing.Size(295, 121);
+            this.btnCheckForUpdate.TabIndex = 1;
+            this.btnCheckForUpdate.Text = "Check For Update";
+            this.btnCheckForUpdate.UseVisualStyleBackColor = true;
+            this.btnCheckForUpdate.Click += new System.EventHandler(this.btnCheckForUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckForUpdate);
+            this.Controls.Add(this.lbVersion);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbVersion;
+        private System.Windows.Forms.Button btnCheckForUpdate;
     }
 }
 
--- MainApp/Form1.cs
+++ MainApp/Form1.cs
@@ -16,5 +16,10 @@ namespace MainApp
         {
             InitializeComponent();
         }
+
+        private void btnCheckForUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
